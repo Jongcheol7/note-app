ff570f7115c940fffa541ed2b2fd0afd86a1d588
Lee JongCheol <whdcjf817@naver.com>
2025-12-20 21:50:41 +0900

    메타데이터 수정

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,19 +8,18 @@ import MainLayout from "@/modules/common/MainLayout";
 export const metadata = {
   title: "NoteApp",
   icons: {
-    icon: "/logo.png", // 또는 "/favicon.png", "/favicon.svg"
+    icon: [{ url: "/logo.png", type: "image/png" }],
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* ✅ 모바일 화면 최적화를 위한 필수 메타 태그 */}
+      {/* <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>NoteApp</title>
         <link rel="icon" href="/logo.png" />
-      </head>
+      </head> */}
       <body>
         <div className="relative flex flex-col mx-auto sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-5xl px-4 sm:px-6 md:px-8 py-3">
           <SessionWrapper>
