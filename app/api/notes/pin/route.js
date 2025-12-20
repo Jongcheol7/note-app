a672d66d33c2308a2d9b776ce0762e699188645e
Lee JongCheol <whdcjf817@naver.com>
2025-12-20 22:29:47 +0900

    pin 업데이트 오류. api route에 prisma 추가

--- app/api/notes/pin/route.js
+++ app/api/notes/pin/route.js
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/route";
 import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
 
 export async function POST(request) {
   const session = await getServerSession(authOptions);
